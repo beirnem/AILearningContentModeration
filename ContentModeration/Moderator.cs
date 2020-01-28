7dccacdfb835291565fa0484fa0a7f7d6bd5cf63
Marcus Beirne <icanseemyhousefromhere@hotmail.com>
2020-01-28 12:12:58 +0000

    Refactor ModertateText to separate class - SRP Minor change to TextFile to bring it back to initial example text

---
+++ ContentModeration/Moderator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ContentModeration
+{
+    public class Moderator
+    {
+        /*
+         * TEXT MODERATION
+         * This example moderates text from file.
+         */
+        public void ModerateText(string inputFile, string outputFile)
+        {
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("TEXT MODERATION");
+            Console.WriteLine();
+            // Load the input text
+            string text = File.ReadAllText(inputFile);
+
+            // Remove carriage returns
+            text = text.Replace(Environment.NewLine, " ");
+            // Convert string to a byte[], then into a stream (for parameter in ScreenText()).
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            MemoryStream stream = new MemoryStream(textBytes);
+
+            Console.WriteLine("Screening {0}...", inputFile);
+            // Format text
+
+            // Save the moderation result to a file.
+            using (StreamWriter outputWriter = new StreamWriter(outputFile, false))
+            {
+                using (var client = Clients.NewClient())
+                {
+                    // Screen the input text: check for profanity, classify the text into three categories,
+                    // do autocorrect text, and check for personally identifying information (PII)
+                    outputWriter.WriteLine("Autocorrect typos, check for matching terms, PII, and classify.");
+
+                    // Moderate the text
+                    var screenResult = client.TextModeration.ScreenText("text/plain", stream, "eng", true, true, null, true);
+                    outputWriter.WriteLine(JsonConvert.SerializeObject(screenResult, Formatting.Indented));
+                }
+
+                outputWriter.Flush();
+                outputWriter.Close();
+            }
+
+            Console.WriteLine($"Results written to {outputFile}");
+            Console.WriteLine();
+        }
+    }
+}
--- ContentModeration/Program.cs
+++ ContentModeration/Program.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Text;
+﻿using System;
 
 namespace ContentModeration
 {
@@ -19,52 +15,10 @@ namespace ContentModeration
         {
             Environment.SetEnvironmentVariable("CONTENT_MODERATOR_ENDPOINT", "https://aicertcontentmoderatortutorial.cognitiveservices.azure.com/");
             Environment.SetEnvironmentVariable("CONTENT_MODERATOR_SUBSCRIPTION_KEY", "2e84cf22ed5e42d99689ea32b33c5ec8");
+
+            Moderator mod = new Moderator();
             // Moderate text from text in a file
-            ModerateText(TextFile, TextOutputFile);
-        }
-
-        /*
-         * TEXT MODERATION
-         * This example moderates text from file.
-         */
-        public static void ModerateText(string inputFile, string outputFile)
-        {
-            Console.WriteLine("-------------------------------------------------------");
-            Console.WriteLine();
-            Console.WriteLine("TEXT MODERATION");
-            Console.WriteLine();
-            // Load the input text
-            string text = File.ReadAllText(inputFile);
-
-            // Remove carriage returns
-            text = text.Replace(Environment.NewLine, " ");
-            // Convert string to a byte[], then into a stream (for parameter in ScreenText()).
-            byte[] textBytes = Encoding.UTF8.GetBytes(text);
-            MemoryStream stream = new MemoryStream(textBytes);
-
-            Console.WriteLine("Screening {0}...", inputFile);
-            // Format text
-
-            // Save the moderation result to a file.
-            using (StreamWriter outputWriter = new StreamWriter(outputFile, false))
-            {
-                using (var client = Clients.NewClient())
-                {
-                    // Screen the input text: check for profanity, classify the text into three categories,
-                    // do autocorrect text, and check for personally identifying information (PII)
-                    outputWriter.WriteLine("Autocorrect typos, check for matching terms, PII, and classify.");
-
-                    // Moderate the text
-                    var screenResult = client.TextModeration.ScreenText("text/plain", stream, "eng", true, true, null, true);
-                    outputWriter.WriteLine(JsonConvert.SerializeObject(screenResult, Formatting.Indented));
-                }
-
-                outputWriter.Flush();
-                outputWriter.Close();
-            }
-
-            Console.WriteLine($"Results written to {outputFile}");
-            Console.WriteLine();
+            mod.ModerateText(TextFile, TextOutputFile);
         }
     }
 }
